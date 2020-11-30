c1caa1da5a2e308bf1366293b72c1e2a43b35af2
Dmi_Krop <melomanko@gmail.com>
2020-11-30 18:07:19 +0700

    Code cleaning

--- lesson7_hw1.py
+++ lesson7_hw1.py
@@ -1,24 +1,7 @@
-# cook_book = {
-#   'Омлет': [
-#     {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-#     {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-#     {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-#     ],
-#   'Утка по-пекински': [
-#     {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-#     {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-#     {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-#     {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-#     ],
-#   'Запеченный картофель': [
-#     {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-#     {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-#     {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-#     ]
-#   }
-
 import os
 from pprint import pprint
+
+FILE_PATH = os.path.join(os.getcwd(), 'recipes.txt')  # Get current workdir
 
 
 def recipes_parse(filename, encoding='utf-8'):
@@ -47,12 +30,11 @@ def recipes_parse(filename, encoding='utf-8'):
                     line = recipes.readline()
                     continue
                 temp_dict = dict(zip(['ingredient_name', 'quantity', 'measure'],
-                                    [i.strip() for i in temp_list]))
+                                     [i.strip() for i in temp_list]))
                 res[recipe_name].append(temp_dict)
             line = recipes.readline()
     return res
 
 
-file_path = os.path.join(os.getcwd(), 'recipes.txt')  # Get current workdir
-cook_book = recipes_parse('recipes.txt')
-pprint(cook_book)
+print('Кулинарная книга:')
+pprint(recipes_parse(FILE_PATH))
